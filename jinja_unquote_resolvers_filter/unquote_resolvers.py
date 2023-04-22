138bad670f71b9d88d402995073c9e2658f838be
Alex Harvey <alex.harvey@foxtel.com.au>
2023-04-22 10:15:42 +1000

    docstrings

--- jinja_unquote_resolvers_filter/unquote_resolvers.py
+++ jinja_unquote_resolvers_filter/unquote_resolvers.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Union
 import yaml
 from yaml.nodes import ScalarNode
 
+from functools import singledispatch
 from textwrap import indent as textwrap_indent
 
 try:
@@ -38,24 +39,38 @@ def unquote_resolvers(
     """
     Remove quotes around resolver expressions and
     produce nice YAML.
+
+    :param data: The input data as a dictionary or list
+      containing resolver expressions.
+    :param indent: The number of spaces to use for indentation
+      of nested structures in the output YAML.  Default is 2
+      spaces.
+    :param output_indent: The number of spaces to use for
+      indentation of the entire output YAML. Default is 0
+      (no indentation).
+
+    :returns: A formatted YAML string with unquoted resolver
+      expressions and the specified indentation settings.
     """
 
+    @singledispatch
     def process_item(item: Any) -> Any:
-        if isinstance(item, str) and item.startswith("!"):
+        return item
+
+    @process_item.register(str)
+    def _(item: str) -> Any:
+        if item.startswith("!"):
             tag_name, tag_body = item.split(" ", 1)
             return Tag(tag_name, tag_body)
-        elif isinstance(item, dict):
-            return process_dict(item)
-        elif isinstance(item, list):
-            return process_list(item)
-        else:
-            return item
+        return item
 
-    def process_dict(dct: Dict) -> Dict:
-        return {key: process_item(value) for key, value in dct.items()}
+    @process_item.register(dict)
+    def _(item: Dict) -> Dict:
+        return {key: process_item(value) for key, value in item.items()}
 
-    def process_list(lst: List) -> List:
-        return [process_item(value) for value in lst]
+    @process_item.register(list)
+    def _(item: List) -> List:
+        return [process_item(value) for value in item]
 
     processed_data = process_item(data)
 
