a6b6b6d4845f3359d824fd549a9ac4efa03319f1
Alex Harvey <alex.harvey@foxtel.com.au>
2023-04-25 01:16:45 +1000

    more

--- jinja_unquote_resolvers_filter/__init__.py
+++ jinja_unquote_resolvers_filter/__init__.py
@@ -8,4 +8,6 @@ class UnquoteResolversFilterExtension(Extension):
         environment.filters["unquote_resolvers"] = self.unquote_resolvers_filter
 
     def unquote_resolvers_filter(self, data, indent=2, output_indent=0, trim=False):
-        return unquote_resolvers(data, indent=indent, output_indent=output_indent, trim=trim)
+        return unquote_resolvers(
+            data, indent=indent, output_indent=output_indent, trim=trim
+        )
